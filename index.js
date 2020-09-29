7eba93198969422ba5b721950f7c71d71ada05c3
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-29 01:50:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  kittens.unshift(name);
+  kittens.push(name);
 }
+
+function
